8abf8952ae002f2dd42a6a5f5715509ad601f892
Sinval <sinvalvieirajunior@gmail.com>
2021-07-10 20:52:16 -0300

    Adding pokemon font

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import { HomeProvider } from './components/pages/Home/home.context';
+import './fonts/pokemon/Pokemon-Solid.ttf';
 
 ReactDOM.render(
 <HomeProvider> <App /> </HomeProvider>,

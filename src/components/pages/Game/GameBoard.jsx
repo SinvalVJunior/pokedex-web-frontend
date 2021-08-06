a6193acfbce8b18b67a21dc07a4ba736ae5b3b46
Sinval <sinvalvieirajunior@gmail.com>
2021-08-06 02:00:09 -0300

    Adding poke API integration

--- src/components/pages/Game/GameBoard.jsx
+++ src/components/pages/Game/GameBoard.jsx
@@ -3,17 +3,17 @@ import GameCard from "./GameCard";
 import { useStyles } from "./Roulette/Roulette.styles";
 
 //A geracao das Gamecards na mão é apenas mockup
-const GameBoard = ({ pokes, selected }) => {
+const GameBoard = ({ pokemons, selected }) => {
   const classes = useStyles();
 
   return (
     <div className={classes.board}>
-      <GameCard poke={pokes[0]} selected={selected[0]}/>
-      <GameCard poke={pokes[1]} selected={selected[1]}/>
-      <GameCard poke={pokes[2]} selected={selected[2]}/>
-      <GameCard poke={pokes[3]} selected={selected[3]}/>
-      <GameCard poke={pokes[4]} selected={selected[4]}/>
-      <GameCard poke={pokes[5]} selected={selected[5]}/>
+      <GameCard pokemon={pokemons[0]} selected={selected[0]}/>
+      <GameCard pokemon={pokemons[1]} selected={selected[1]}/>
+      <GameCard pokemon={pokemons[2]} selected={selected[2]}/>
+      <GameCard pokemon={pokemons[3]} selected={selected[3]}/>
+      <GameCard pokemon={pokemons[4]} selected={selected[4]}/>
+      <GameCard pokemon={pokemons[5]} selected={selected[5]}/>
     </div>
   );
 };

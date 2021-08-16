27a58d276b5ee497a0b88e4dba26924ad1c1e8f0
Sinval <sinvalvieirajunior@gmail.com>
2021-08-15 21:35:24 -0300

    Adding inventory integration with POKE API and the Database

--- src/clients/backend.js
+++ src/clients/backend.js
@@ -27,48 +27,15 @@ export const postLogin = async (email, password) => {
 }
 
 export const getInventory = async (userId) => {
-
-  return [
-    {
-      name: 'bulbasaur',
-      stats: {
-        hp: 45,
-        attack: 49,
-        specialAttack: 65,
-        defence: 49,
-        specialDefence: 65,
-        speed: 45
-      },
-      species: {
-        curiosity: "A strange seed was\nplanted on its\nback at birth.\fThe plant sprouts\nand grows with\nthis POKÉMON.",
-        color: "green",
-        baseHappiness: 70,
-        captureRate: 45,
-        evolvesFrom: "",
-        evolvesTo: "ivysaur",
-        habitat: "grassland",
-        hasGenderDifferences: false,
-        isMythical: false,
-        isLegendary: false,
-        shape: 'quadruped',
-        img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
-      },
-      imgUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/1.gif'
-    },
-    {
-      name: 'ivysaur',
-      stats: {
-        hp: 60,
-        attack: 62,
-        specialAttack: 80,
-        defence: 63,
-        specialDefence: 80,
-        speed: 60
-      },
-      imgUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/2.gif'
-    },
-
-  ];
+  try {
+    const header = {
+      'Content-Type': 'application/json'
+    }
+    const response = await axios.get(`${backendUrl}/inventory?userId=${userId}`, { headers: header }).catch(err => err.response);
+    return response.data;
+  } catch (err) {
+    alert(err)
+  }
 };
 
 export const editUser = (email,name,username,password) =>{

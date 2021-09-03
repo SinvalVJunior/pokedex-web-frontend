63d819ae8fbdc5e675a55fb468fda216f4283e30
Sinval <sinvalvieirajunior@gmail.com>
2021-09-03 15:47:55 -0300

    Adding token to header call

--- src/clients/backend.js
+++ src/clients/backend.js
@@ -3,12 +3,19 @@ const axios = require('axios');
 const backendUrl = process.env.REACT_APP_API_URL;
 
 
+const getToken = () => {
+  return localStorage.getItem("token");
+}
+
+const getBasicHeader = () => ({
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer ${getToken()}`
+});
+
 export const getUserInfo = async () => {
   try {
-    const header = {
-      'Content-Type': 'application/json'
-    }
-    const response = await axios.get(`${backendUrl}/users`, { headers: header }).catch(err => err.response);
+    console.log(getBasicHeader());
+    const response = await axios.get(`${backendUrl}/users`, { headers: getBasicHeader() }).catch(err => err.response);
     return response.data;
   } catch (err) {
     alert(err)
@@ -17,7 +24,7 @@ export const getUserInfo = async () => {
 
 export const postLogin = async (email, password) => {
   try {
-    const response = await axios.post(`${backendUrl}/login`, { email: email, password: password }).catch(err => err.response);
+    const response = await axios.post(`${backendUrl}/login`, { email: email, password: password, headers: getBasicHeader() }).catch(err => err.response);
 
     if(response.status === 200) return {...response.data, error: false};
     else throw new Error(response.data?.error);
@@ -29,7 +36,7 @@ export const postLogin = async (email, password) => {
 
 export const postLoginFacebook = async (email, name, picture) => {
   try {
-    const response = await axios.post(`${backendUrl}/login/facebook`, { email, name, picture }).catch(err => err.response);
+    const response = await axios.post(`${backendUrl}/login/facebook`, { email, name, picture, headers: getBasicHeader() }).catch(err => err.response);
 
     if(response.status === 200) return {...response.data, error: false};
     else throw new Error(response.data?.error);
@@ -41,10 +48,7 @@ export const postLoginFacebook = async (email, name, picture) => {
 
 export const getInventory = async (userId) => {
   try {
-    const header = {
-      'Content-Type': 'application/json'
-    }
-    const response = await axios.get(`${backendUrl}/inventory?userId=${userId}`, { headers: header }).catch(err => err.response);
+    const response = await axios.get(`${backendUrl}/inventory?userId=${userId}`, { headers: getBasicHeader()} ).catch(err => err.response);
     return response.data;
   } catch (err) {
     alert(err)
@@ -53,7 +57,7 @@ export const getInventory = async (userId) => {
 
 export const createUser = async (email, name, password) =>{
   try{
-    const response = await axios.post(`${backendUrl}/users`, { email: email, name: name, password: password }).catch(err => err.response);
+    const response = await axios.post(`${backendUrl}/users`, { email: email, name: name, password: password, headers: getBasicHeader() }).catch(err => err.response);
 
     if(response.status === 200) return {...response.data, error: false};
     else throw new Error(response.data?.error);
@@ -65,7 +69,7 @@ export const createUser = async (email, name, password) =>{
 
 export const editUser = async (userId,name) =>{
   try{
-    const response = await axios.put(`${backendUrl}/users`, { userId: userId, name: name}).catch(err => err.response);
+    const response = await axios.put(`${backendUrl}/users`, { userId: userId, name: name, headers: getBasicHeader()}).catch(err => err.response);
 
     if(response.status === 200) return {...response.data, error: false};
     else throw new Error(response.data?.error);
@@ -79,7 +83,7 @@ export const editUser = async (userId,name) =>{
 export const getPokemons = async (numberOfPokemons) => {
 
   try {
-    const response = await axios.get(`${backendUrl}/pokemon?numberOfPokemons=${numberOfPokemons}`).catch(err => err.response);
+    const response = await axios.get(`${backendUrl}/pokemon?numberOfPokemons=${numberOfPokemons}`, {headers: getBasicHeader()}).catch(err => err.response);
 
     if(response.status === 200) return response.data;
     else throw new Error(response.data?.error);
@@ -92,7 +96,7 @@ export const getPokemons = async (numberOfPokemons) => {
 export const addPokemonToInventory = async ( userId, pokemonId) => {
 
 try {
-    const response = await axios.post(`${backendUrl}/inventory`, { userId, pokemonId }).catch(err => err.response);
+    const response = await axios.post(`${backendUrl}/inventory`, { userId, pokemonId, headers: getBasicHeader() }).catch(err => err.response);
 
     if(response.status === 200) return {...response.data, error: false};
     else throw new Error(response.data?.error);
